820b7224c1553688da3e8283f7b5ae672b14ffed
Firecraker <mezamic000@gmail.com>
2020-06-01 13:23:50 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# liri-node-app
+Liri-Node-App
+
+---
+
+This liri-node-app searches for Spotify songs, Bands in Town for concerts, and OMDB for movies. This helps users identify songs, find concerts, and info on movies.
+
+
+Instructions on how to run the app:
+(Note: You will need to run this app from the command line)
+
+1. First you need to navigate into the liri.js file.
+2. Enter into the node system in your terminal by entering \$.
+3. Input the information that you wish to get information on.
+
+   - For a spotify song input:
+
+     - node liri.js spotify-this=song [song name]
+
+   - For concert info input:
+
+     - node liri.js concert-this [artist/band name]
+
+   - For movie information input:
+     - node liri.js movie-this what about bob
+
+
+
+Link to the deployed version of the app:
+https://github.com/mezamic000/liri-node-app
+
+
+Technologies used in the app:
+- axios 
+- omdb
+- node-spotify-api
+
+
+This app was created by Michelle Meza.
--- liri.js
+++ liri.js
@@ -1,3 +1,122 @@
 require("dotenv").config();
+var keys = require("./keys.js");
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
+var axios = require("axios");
+var moment = require("moment");
+var fs = require("fs");
 
+var command = process.argv[2];
+var input = process.argv[3];
 
+var defaultSong = "The Sign";
+var defaultMovie = "Mr. Nobody";
+
+switch (command) {
+	case "concert-this":
+		getBands(input);
+		break;
+	case "spotify-this-song":
+		getSongs(input);
+		break;
+	case "movie-this":
+		getMovies(input);
+		break;
+	case "do-what-it-says":
+		doWhatItSays(input);
+		break;
+	default:
+		break;
+}
+
+function getBands(artist) {
+	var artist = input;
+
+	axios
+		.get(
+			"https://rest.bandsintown.com/artists/" +
+				artist +
+				"/events?app_id=codingbootcamp"
+		)
+		.then(function (response) {
+			// console.log(response.data);
+			console.log("Name of venue: " + response.data[0].venue.name);
+			console.log("Venue location: " + response.data[0].venue.city);
+			// Format using moment
+			// console.log(response.data[0].datetime);
+			var eDate = moment(response.data[0].datetime).format("MM/DD/YYYY");
+			console.log("Date of the Event: " + eDate);
+		})
+		.catch(function (error) {
+			console.log(error);
+		});
+}
+
+function getSongs(song) {
+	// var song = input
+
+	spotify
+		.search({ type: "track", query: song })
+		.then(function (data) {
+			if (song === "") {
+				song = "The Sign";
+			}
+			// console.log(data.tracks.items[0]);
+			console.log("Artist: ", data.tracks.items[0].album.artists[0].name);
+			console.log(
+				"Preview Song: " + data.tracks.items[0].external_urls.spotify
+			);
+			console.log("Album Name: " + data.tracks.items[0].album.name);
+		})
+		.catch(function (err) {
+			console.log(err);
+		});
+}
+
+function getMovies(movie) {
+	var movie = input;
+
+	if (movie === "") {
+		movie = defaultMovie;
+	}
+
+	axios
+		.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy")
+		.then(function (response) {
+			// console.log(response.data)
+			console.log("\n=====================================\n");
+			console.log("Movie title: " + response.data.Title);
+			console.log("Year released: " + response.data.Year);
+			console.log("IMDB rating: " + response.data.Ratings[0].Value);
+			console.log("Rotten Tomatoes rating: " + response.data.Ratings[1].Value);
+			console.log("Country movie was produced in: " + response.data.Country);
+			console.log("Language of the movie: " + response.data.Language);
+			console.log("Plot: " + response.data.Plot);
+			console.log("Actors: " + response.data.Actors);
+			console.log("\n====================================\n");
+		});
+}
+
+function doWhatItSays() {
+	fs.readFile("random.txt", "utf8", function (error, data) {
+		if (error) {
+			return console.log(error);
+		}
+		data = data.split(",");
+
+		command = data[0];
+		input = data[1];
+
+		switch (command) {
+			case "concert-this":
+				getBands(input);
+				break;
+			case "spotify-this-song":
+				getSongs(input);
+				break;
+			case "movie-this":
+				getMovies(input);
+				break;
+		}
+	});
+}
